8370663b29ce13bc316176e9cb8c658cc089cc99
vaibhav <97797655+Frostbite-ai@users.noreply.github.com>
2023-11-20 14:06:29 +0530

    Update Hill_Climbng_GUI.py

--- Hill_Climbng_GUI.py
+++ Hill_Climbng_GUI.py
@@ -1,79 +1,54 @@
-#hill climbing
-
-import tkinter as tk
-# import pygame
 import random
 import matplotlib.pyplot as plt
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-
-# Initialize Pygame
-# pygame.init()
-
-
 
 def objective_function(x):
+    """
+    Objective function to be maximized.
+    """
     return -x ** 2 + 10 * x + 20
 
-
-# Hill climbing function
 def hill_climbing(max_iterations, step_size):
+    """
+    Hill Climbing algorithm: Iteratively explores neighboring nodes and chooses the best one.
+    """
     x = random.uniform(0, 10)
     path = [(x, objective_function(x))]  # Store the optimization path
 
-    for _ in range(max_iterations):
+    print(f"Initial position: x = {x}, Value = {objective_function(x)}")
+
+    for i in range(max_iterations):
         current_value = objective_function(x)
         neighbor_x = x + random.uniform(-step_size, step_size)
         neighbor_value = objective_function(neighbor_x)
 
+        print(f"Iteration {i+1}: x = {neighbor_x}, Value = {neighbor_value}")
+
         if neighbor_value > current_value:
             x = neighbor_x
             path.append((x, neighbor_value))  # Store the new position and value
+            print(f"New best found: x = {x}, Value = {neighbor_value}")
 
     return path
 
+# User input and instructions
+print("Hill Climbing Algorithm to maximize the function -x^2 + 10x + 20")
+print("Enter the number of iterations and the step size for the algorithm.")
 
-# Function to start the optimization
-def start_optimization():
-    max_iterations = int(iterations_entry.get())
-    step_size = float(step_size_entry.get())
+# Collecting user inputs
+try:
+    max_iterations = int(input("Enter the number of iterations: "))
+    step_size = float(input("Enter the step size: "))
+except ValueError:
+    print("Invalid input. Please enter valid numbers.")
+    exit()
 
-    path = hill_climbing(max_iterations, step_size)
+# Run the Hill Climbing algorithm
+path = hill_climbing(max_iterations, step_size)
 
-    # Plot the optimization path using Matplotlib
-    fig, ax = plt.subplots()
-    x_values, y_values = zip(*path)
-    ax.plot(x_values, y_values, marker='o', linestyle='-')
-    ax.set_xlabel('x')
-    ax.set_ylabel('Objective Value')
-    ax.set_title('Hill Climbing Optimization Path')
-
-    # Embed the Matplotlib plot in Tkinter using FigureCanvasTkAgg
-    canvas = FigureCanvasTkAgg(fig, master=root)
-    canvas_widget = canvas.get_tk_widget()
-    canvas_widget.pack()
-
-
-# Create the main Tkinter window
-root = tk.Tk()
-root.title("Hill Climbing Optimization with Matplotlib")
-
-# Label and entry for maximum iterations
-iterations_label = tk.Label(root, text="Max Iterations:")
-iterations_label.pack()
-iterations_entry = tk.Entry(root)
-iterations_entry.pack()
-iterations_entry.insert(0, "100")
-
-# Label and entry for step size
-step_size_label = tk.Label(root, text="Step Size:")
-step_size_label.pack()
-step_size_entry = tk.Entry(root)
-step_size_entry.pack()
-step_size_entry.insert(0, "0.1")
-
-# Button to start optimization
-start_button = tk.Button(root, text="Start Optimization", command=start_optimization)
-start_button.pack()
-
-# Run the Tkinter main loop
-root.mainloop()
+# Plotting the path using Matplotlib
+x_values, y_values = zip(*path)
+plt.plot(x_values, y_values, marker='o', linestyle='-')
+plt.xlabel('x')
+plt.ylabel('Objective Value')
+plt.title('Hill Climbing Optimization Path')
+plt.show()
